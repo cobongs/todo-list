4595420bda59bd93a880427c3864ce73b91248dc
angelina <eunkyong82@snaps.kr>
2019-06-10 11:52:48 +0900

    activeIndex 추가

--- src/components/BtnsWrapper.js
+++ src/components/BtnsWrapper.js
@@ -4,11 +4,15 @@ class BtnsWrapper extends React.Component {
 
   render () {
 
+    const { activeIndex} = this.props
+
     return (
       <div>
         {
           this.props.tab.map((item, idx) => {
             return <button key={idx}
+                           className={idx === activeIndex ? 'active' : '' }
+                           data-index={idx}
                            onClick={() => this.props.callback(idx)}>
               {item}
             </button>
